0b511fabb69fb309b3d252d802dde1d0857ab271
sunnypo <46382526+sunnypo@users.noreply.github.com>
2021-11-02 14:51:30 -0400

    Update CreateFibonacciSquence.py
    bug fixed- pivot point error

--- BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
+++ BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
@@ -4,5 +4,6 @@ from maya import cmds
 for x in range(0,300):
     cmds.polyCube(w=1, h=1, d=1, name='gugu%s'%(x))
     cmds.select('gugu%s'%(x))
-    cmds.rotate(0, 137.51*x, 0)
     cmds.move(x*0.05+1, 0, 0)
+    cmds.move(0, 0, 0, 'gugu%s.scalePivot'%(x), 'gugu%s.rotatePivot'%(x))
+    cmds.rotate(0, 137.51*x, 0)

1823bdf81bf231cd60d7344f667fafca1c6e32b2
sunnypo <46382526+sunnypo@users.noreply.github.com>
2021-11-02 12:19:35 -0400

    Update CreateFibonacciSquence.py
    changed the array option int o range(int, int)
    simplified step

--- BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
+++ BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
@@ -1,12 +1,8 @@
 from maya import cmds
 
-duplicateTime = [1,2,3,4,5,6,7,8,9,10]
 
-
-for x in duplicateTime:
+for x in range(0,300):
     cmds.polyCube(w=1, h=1, d=1, name='gugu%s'%(x))
     cmds.select('gugu%s'%(x))
-    cmds.move(x*0.05+1, 0, 0)
-    cmds.move(0, 0, 0, 'gugu%s.scalePivot'%(x), 'gugu%s.rotatePivot'%(x))
     cmds.rotate(0, 137.51*x, 0)
-#    cmds.select(clear)
+    cmds.move(x*0.05+1, 0, 0)

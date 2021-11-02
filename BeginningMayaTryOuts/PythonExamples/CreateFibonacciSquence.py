9601b2512b50ca255d948ce9c88ffe8d4c961ab0
sunnypo <46382526+sunnypo@users.noreply.github.com>
2021-11-02 17:18:05 -0400

    Update CreateFibonacciSquence.py
    you can custom the input object now
    does not create object every time, instead, it duplicate from last result

--- BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
+++ BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
@@ -1,9 +1,13 @@
 from maya import cmds
 
+#you need a origional object at 1 0 0 to begin, and the name have to be gugu0
 
-for x in range(0,300):
-    cmds.polyCube(w=1, h=1, d=1, name='gugu%s'%(x))
+for x in range(1,101):
+
+    cmds.duplicate('gugu%s'%(x-1))
     cmds.select('gugu%s'%(x))
-    cmds.move(x*0.05+1, 0, 0)
+    cmds.move(0.05, 0.05, 0, relative=True)
     cmds.move(0, 0, 0, 'gugu%s.scalePivot'%(x), 'gugu%s.rotatePivot'%(x))
-    cmds.rotate(0, 137.51*x, 0)
+ 
+for x in range(1,101):    
+    cmds.rotate(0, 137.51*x, 0, 'gugu%s'%(x))

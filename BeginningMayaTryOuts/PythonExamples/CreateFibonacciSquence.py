4a7ff97505a3165fd4f005eda01f360540dac987
sunnypo <46382526+sunnypo@users.noreply.github.com>
2021-11-03 12:25:20 -0400

    lotus version-flower

--- BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
+++ BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
@@ -1,18 +1,18 @@
 from maya import cmds
 
-#you need a origional object at 1 0 0 to begin, and the name have to be gugu0
+#you need a origional object to begin, and the name have to be gugu0
 
 rotateAngle=0
 
 
-for x in range(1,301):
+for x in range(1,21):
     cmds.duplicate('gugu%s'%(x-1))
     cmds.move(0, x*0.0005, 0, 'gugu%s'%(x))
-    cmds.rotate(0, 0, -x*.23, 'gugu%s'%(x))
-    cmds.scale(10-(x*x/10000), 10-(x*x/10000), 10-(x*x/10000), 'gugu%s'%(x))
+    cmds.rotate(-x*4, 0, 0, 'gugu%s'%(x))
+    cmds.scale(.9-(x*x/10000), .9-(x*x/10000), .9-(x*x/10000), 'gugu%s'%(x))
     cmds.move(0, 0, 0, 'gugu%s.scalePivot'%(x), 'gugu%s.rotatePivot'%(x))
  
-for x in range(1,301):
+for x in range(1,21):
     rotateAngle=rotateAngle+137.51
     if rotateAngle>=360:
         rotateAngle=rotateAngle-360

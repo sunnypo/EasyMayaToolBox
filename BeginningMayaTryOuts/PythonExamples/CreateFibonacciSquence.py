89bbd624ae6b62e57025237c96455bf8672adf10
sunnypo <46382526+sunnypo@users.noreply.github.com>
2021-11-03 09:50:43 -0400

    flowerVersion
    now it spins both y and z
    and scale smaller when it closer to the middle
    300 object works best for this version

--- BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
+++ BeginningMayaTryOuts/PythonExamples/CreateFibonacciSquence.py
@@ -2,12 +2,18 @@ from maya import cmds
 
 #you need a origional object at 1 0 0 to begin, and the name have to be gugu0
 
-for x in range(1,101):
+rotateAngle=0
 
+
+for x in range(1,301):
     cmds.duplicate('gugu%s'%(x-1))
-    cmds.select('gugu%s'%(x))
-    cmds.move(0.05, 0.05, 0, relative=True)
+    cmds.move(0, x*0.0005, 0, 'gugu%s'%(x))
+    cmds.rotate(0, 0, -x*.23, 'gugu%s'%(x))
+    cmds.scale(10-(x*x/10000), 10-(x*x/10000), 10-(x*x/10000), 'gugu%s'%(x))
     cmds.move(0, 0, 0, 'gugu%s.scalePivot'%(x), 'gugu%s.rotatePivot'%(x))
  
-for x in range(1,101):    
-    cmds.rotate(0, 137.51*x, 0, 'gugu%s'%(x))
+for x in range(1,301):
+    rotateAngle=rotateAngle+137.51
+    if rotateAngle>=360:
+        rotateAngle=rotateAngle-360
+    cmds.rotate(0, rotateAngle, 0, 'gugu%s'%(x),relative=True)

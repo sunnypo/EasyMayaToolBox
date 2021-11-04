6cf4cf2115e61d4094911a90a619b9c417c6135c
sunnypo <46382526+sunnypo@users.noreply.github.com>
2021-11-04 17:36:31 -0400

    Update renameMuntipleItem.py
    more friendly version uwu

--- BeginningMayaTryOuts/PythonExamples/renameMuntipleItem.py
+++ BeginningMayaTryOuts/PythonExamples/renameMuntipleItem.py
@@ -2,5 +2,17 @@ from maya import cmds
 
 #you can modify the range, name before and name after
 
-for i in range(1,21):
-    cmds.rename('gugu%s'%(i),'lotus_flower%s'%(i))
+rangeBeagin=0   #change the zero into the number that you want to start rename
+rangeEnd=0  #change the zero into the number that you want to end rename
+
+oldName='pCube'
+newName='renamedObject'
+#for those two number, enter the number range that you want to modify
+#must be an integer, can start from not zero
+
+#for the names, you enter the old name without the number at the end 
+#and enter the name you want to rename to your object
+#keep everything in the ''
+
+for i in range(rangeBeagin,rangeEnd+1):
+    cmds.rename('oldName%s'%(i),'newName%s'%(i))
